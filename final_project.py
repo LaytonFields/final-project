351b89fcb784249f7bb578f10c1fda1da18e5cfc
SethWhitt <100959288+SethWhitt@users.noreply.github.com>
2022-04-18 11:50:35 -0500

    Update final_project.py

---
+++ final_project.py
@@ -0,0 +1,25 @@
+import pygame, pgzrun, numpy, os
+
+#Setup and Variables--------------------------------------------
+
+#Screen Size
+WIDTH = 1000
+HEIGHT = 1000
+
+#Variables
+Game_over == False
+Game_complete == False
+
+#Actors
+Doomguy = 
+
+
+
+
+
+
+
+
+
+
+pgzrun.go()
