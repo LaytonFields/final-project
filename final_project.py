5c96d680f45766773aa4115286c7e5a28ab8fccf
SethWhitt <100959288+SethWhitt@users.noreply.github.com>
2022-04-26 11:54:45 -0500

    update 1.5

--- final_project.py
+++ final_project.py
@@ -15,19 +15,32 @@ START_POS = 250,400
 Doomguy = Actor("doomguy.png")
 Doomguy.pos = START_POS
 
-def draw():
-    screen.blit("background.jpg", (0,0))
-    Doomguy.draw()
 
+
+#Doomguy shooting---------------------------------------------
+def shoot():
+    #if 
+    pass
+
+#Doomguy movement---------------------------------------------
 def DG_Walk():
     if keyboard.a and Doomguy.x > 0:
         Doomguy.x -=5
     elif keyboard.d and Doomguy.x < WIDTH:
         Doomguy.x +=5
 
+def DG_Walk_animation():
+    global Doomguy
+    if keyboard.a:
+        Doomguy("doomguy.png".image) == "doomguy_walk1.png"
+
+def draw():
+    screen.blit("background.jpg", (0,0))
+    Doomguy.draw()
+
 def update():
-#Doomguy movement---------------------------------------------
     DG_Walk()
+    DG_Walk_animation()
 
 
 
