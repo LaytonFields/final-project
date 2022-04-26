444f65c966f3f442fb48730e0def6fed2bdf06e8
SethWhitt <100959288+SethWhitt@users.noreply.github.com>
2022-04-26 11:30:29 -0500

    update 1.0

--- final_project.py
+++ final_project.py
@@ -3,19 +3,31 @@ import pygame, pgzrun, numpy, os
 #Setup and Variables--------------------------------------------
 
 #Screen Size
-WIDTH = 1000
-HEIGHT = 1000
+WIDTH = 1500
+HEIGHT = 500
 
 #Variables
-Game_over == False
-Game_complete == False
+game_over = False
+game_complete = False
+START_POS = 250,400
 
 #Actors
-Doomguy = 
+Doomguy = Actor("doomguy.png")
+Doomguy.pos = START_POS
 
+def draw():
+    screen.blit("background.jpg", (0,0))
+    Doomguy.draw()
 
+def DG_Walk():
+    if keyboard.a and Doomguy.x > 0:
+        Doomguy.x -=5
+    elif keyboard.d and Doomguy.x < WIDTH:
+        Doomguy.x +=5
 
-
+def update():
+#Doomguy movement---------------------------------------------
+    DG_Walk()
 
 
 
