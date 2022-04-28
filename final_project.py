3c2add13028fc2be279638a53d5ac0f87cda569c
SethWhitt <100959288+SethWhitt@users.noreply.github.com>
2022-04-28 11:54:15 -0500

    sdsadasd

--- final_project.py
+++ final_project.py
@@ -1,50 +1,54 @@
 import pygame, pgzrun, numpy, os
 
-#Setup and Variables--------------------------------------------
+START_POS = 250,400
+doomguy_model = "doomguy.png"
+bullet = "bullet.png"
 
 #Screen Size
 WIDTH = 1500
 HEIGHT = 500
 
+#Doomguy ---------------------------------------------
+class Doomguy():
+
+    def DG_Walk_animation(self):        
+        if keyboard.d:
+            doomguy_model == ("doomguy_walk1.png")
+
+    def DG_walk(self):
+        if keyboard.a and self.actor.x > 0:
+            self.actor.x -=5
+        elif keyboard.d and self.actor.x < WIDTH:
+            self.actor.x +=5
+
+    def __init__(self):
+        self.actor = Actor(doomguy_model)
+        self.actor.pos = (START_POS)
+
+        self.bullet = Actor(bullet)
+        self.bullet.pos = (START_POS)
+
+    def shoot(self):
+        if keyboard.q:
+            self.bullet.draw
+
+#Setup and Variables--------------------------------------------
+
+
 #Variables
 game_over = False
 game_complete = False
-START_POS = 250,400
+DG = Doomguy()
 
-#Actors
-Doomguy = Actor("doomguy.png")
-Doomguy.pos = START_POS
-
-
-
-#Doomguy shooting---------------------------------------------
-def shoot():
-    #if 
-    pass
-
-#Doomguy movement---------------------------------------------
-def DG_Walk():
-    if keyboard.a and Doomguy.x > 0:
-        Doomguy.x -=5
-    elif keyboard.d and Doomguy.x < WIDTH:
-        Doomguy.x +=5
-
-def DG_Walk_animation():
-    global Doomguy
-    if keyboard.a:
-        Doomguy("doomguy.png".image) == "doomguy_walk1.png"
 
 def draw():
     screen.blit("background.jpg", (0,0))
-    Doomguy.draw()
+    DG.actor.draw()
 
 def update():
-    DG_Walk()
-    DG_Walk_animation()
-
-
-
-
+    DG.DG_walk()
+    DG.DG_Walk_animation()
+    DG.shoot()
 
 
 pgzrun.go()
